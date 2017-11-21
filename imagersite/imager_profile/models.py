7e583b0b8d644c548fd15b45e417ee620da24a3b
maxawolff <maxawolff@hotmail.com>
2017-11-20 17:08:36 -0800

    added repr to profile

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -23,12 +23,12 @@ class ImagerProfile(models.Model):
                         ['CEL', 'Celebrations'])
     website = models.CharField(max_length=100)
     location = models.CharField(max_length=50)
-    bio = models.CharField(max_lenght=500)
-    phone = models.CharField(max_lenght=10)
+    bio = models.CharField(max_length=500)
+    phone = models.CharField(max_length=10)
     fee = models.FloatField()
     active = ActiveProfileManager()
-    services = models.CharField(choices=SERVICES_CHOICES)
-    photo_styles = models.CharField(choices=STYLE_CHOICES)
+    services = models.CharField(max_length=30, choices=SERVICES_CHOICES)
+    photo_styles = models.CharField(max_length=30, choices=STYLE_CHOICES)
     user = models.OneToOneField(User, related_name='profile',
                                 on_delete=models.CASCADE)
 
@@ -36,3 +36,7 @@ class ImagerProfile(models.Model):
     def is_active(self):
         """."""
         return self.user.is_active
+
+    def __repr__(self):
+        """."""
+        print(self.user.first_name + ', ' + self.bio)
