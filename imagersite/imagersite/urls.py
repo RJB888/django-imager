7a06f9340e0253c4f08bdae105414b9a53ae5db6
maxawolff <maxawolff@hotmail.com>
2017-11-21 17:18:56 -0800

    working on registration, I think

--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from imager_profile import views
+from django.contrib.auth import views as log_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home_view, name='homepage')
+    url(r'^$', views.home_view, name='homepage'),
+    url(r'^accounts/', include('registration.backends.hmac.urls')),
+    url(r'^login/', log_views.LoginView)
 ]
