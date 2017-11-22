1f67c12f400845654fad628cb6cfc47ef355364a
Rob Bronson <robert.j.bronson@gmail.com>
2017-11-22 14:54:14 -0800

    added models, and updated migrations

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -14,29 +14,24 @@ PUBLISHED_CHOICES = (
 class Photo(models.Model):
     """Do some modeling."""
 
-    import Album
-    import PUBLISHED_CHOICES
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=120)
     date_uploaded = models.DateField()
     date_modified = models.DateField()
     date_published = models.DateField()
     published = models.CharField(max_length=15, choices=PUBLISHED_CHOICES)
-    user = models.OneToOne(User, on_delete=models.CASCADE)
-    album = models.ManyToManyField(Album)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
 class Album(models.Model):
     """Do some albuming."""
 
-    import PUBLISHED_CHOICES
-    import Photo
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=120)
     date_uploaded = models.DateField()
     date_modified = models.DateField()
     date_published = models.DateField()
     published = models.CharField(max_length=15, choices=PUBLISHED_CHOICES)
-    user = models.OneToOne(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     photo = models.ManyToManyField(Photo)
-    cover = models.OneToOne(Photo)
+    cover = models.OneToOneField(Photo, related_name='+')
