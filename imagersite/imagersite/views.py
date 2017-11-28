58007a1d207d84c3597047de006af25901a6b6cf
maxawolff <maxawolff@hotmail.com>
2017-11-27 17:59:15 -0800

    added three random published images to the home page

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from imager_images.models import Photo
 import pdb
+import random
 
 # Create your views here.
 
 
 def home_view(request):
     """."""
-    photos = Photo.objects.get()
-    return render(request, 'imagersite/base.html', {'images': photos})
+    photos = Photo.objects.all()
+    published = []
+    for photo in photos:
+        if photo.published == "PBL":
+            published.append(photo)
+    carousel_list = []
+    if len(published) >= 3:
+        while len(carousel_list) < 3:
+            index = random.randint(0, len(published) - 1)
+            image = published[index]
+            if image not in carousel_list:
+                carousel_list.append(image)
+    else:
+        for image in published:
+            carousel_list.append(image)
+    return render(request, 'imagersite/base.html', {'images': carousel_list})
