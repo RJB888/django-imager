9c588bb7840bad5874f44d649b0a036b866a60c8
maxawolff <maxawolff@hotmail.com>
2017-12-09 11:54:42 -0800

    fixed tests on profile and removed null field from album

--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -12,24 +12,31 @@ class ProfileTestCase(TestCase):
     """."""
 
     def setup(self):
-        bob = User()
-        bob.username = "Joe"
-        bob_profile = ImagerProfile()
-        bob_profile.user = bob
-        bob = User()
-        bob_profile.website = "James.com"
-        jim.username = "Jeff"
-        jim_profile = ImagerProfile()
-        jim_profile.user = jim
-        profile1 = ImagerProfile.active.create()
+        pass
 
     def test_imager_profile_class(self):
         """."""
-        profile = ImagerProfile.active.get(website="James.com")
-        self.assertEqual(profile.user.username, "Joe")
+        User.objects.create(username='Bob')
+        user1 = User.objects.get(username='Bob')
+        ImagerProfile.active.create(user=user1, website='bob.com', location='here', bio='who', phone='phone', fee=10, services='WD', photo_styles='BW')
+
+        User.objects.create(username='Joe')
+        user2 = User.objects.get(username='Joe')
+        ImagerProfile.active.create(user=user2, website='joe.com', location='there', bio='cares', phone='phone', fee=10, services='WD', photo_styles='BW')
+        profile = ImagerProfile.active.get(website="bob.com")
+        self.assertEqual(profile.user.username, "Bob")
+        self.assertEqual(profile.location, "here")
 
     def test_user_is_active(self):
         """."""
-        profile = ImagerProfile.active.get(website="James.com")
+        User.objects.create(username='Bob')
+        user1 = User.objects.get(username='Bob')
+        ImagerProfile.active.create(user=user1, website='bob.com', location='here', bio='who', phone='phone', fee=10, services='WD', photo_styles='BW')
+
+        User.objects.create(username='Joe')
+        user2 = User.objects.get(username='Joe')
+        ImagerProfile.active.create(user=user2, website='joe.com', location='there', bio='cares', phone='phone', fee=10, services='WD', photo_styles='BW')
+        profile = ImagerProfile.active.get(website="bob.com")
+        profile = ImagerProfile.active.get(website="joe.com")
 
         self.assertEqual(profile.is_active, True)
