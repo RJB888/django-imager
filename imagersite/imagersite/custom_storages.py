ffa66c2dbad4c50348fbc6425b47923540a85aab
maxawolff <maxawolff@hotmail.com>
2017-12-08 16:00:14 -0800

    s3 settings for deployment

---
+++ imagersite/imagersite/custom_storages.py
@@ -0,0 +1,11 @@
+"""Custom storage classes for static and media files."""
+from django.conf import settings
+from storages.backends.s3boto import S3BotoStorage
+
+
+class StaticStorage(S3BotoStorage):
+    location = settings.STATICFILES_LOCATION
+
+
+class MediaStorage(S3BotoStorage):
+    location = settings.MEDIAFILES_LOCATION
