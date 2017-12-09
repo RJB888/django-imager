e869a525d0405a1cf530707e3fac7a0602ceb49e
maxawolff <maxawolff@hotmail.com>
2017-12-08 16:20:17 -0800

    fixing typos

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -147,7 +147,7 @@ if DEBUG:
 
 else:
     AWS_STORAGE_BUCKET_NAME = 'django-rob-max'
-    AWS_ACCES_KEY_ID = os.environ.get('AWS_ACCES_KEY_ID')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
 
