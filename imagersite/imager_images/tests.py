9c585a28be7344d9db5af64e62aafe00cb993fb7
maxawolff <maxawolff@hotmail.com>
2017-12-13 12:08:06 -0800

    all tests passing

--- imagersite/imager_images/tests.py
+++ imagersite/imager_images/tests.py
@@ -60,9 +60,10 @@ class ImagesTestCase(TestCase):
         """Make object."""
         print('this ran')
         user1 = UserFactory.create(username="Bob", first_name='Bob', last_name='Boberts')
-        profile1 = ProfileFactory.create(user=user1, website='test', location='here', bio='isuck', phone='phone', fee=10, services='WD', photo_styles='BW')
+        profile1 = ImagerProfile.active.get(user=user1)
+        # profile1 = ProfileFactory.create(user=user1, website='test', location='here', bio='isuck', phone='phone', fee=10, services='WD', photo_styles='BW')
         user2 = UserFactory.create(username="Jim")
-        profile2 = ProfileFactory.create(user=user2, website='google', location='here', bio='isuck', phone='phone', fee=10, services='WD', photo_styles='BW')
+        profile2 = ImagerProfile.active.get(user=user2)
         photo1 = PhotoFactory.create(title='title', description='Elephant', published='PBL', user=profile1, image='https://i.vimeocdn.com/portrait/58832_300x300')
 
         photo2 = PhotoFactory.create(title='White Elephant', description='in the room', published='PVT', user=profile1, image='https://pixabay.com/p-361514/?no_redirect')
@@ -74,201 +75,198 @@ class ImagesTestCase(TestCase):
                               published="PVT", user=profile1)
         album2.photo.add(photo2)
 
-    def test_sample_test(self):
-        """Debugging."""
-        pdb.set_trace()
-        self.assertTrue(True)
 
-    # def test_setup(self):
-    #     """."""
-    #     self.assertTrue(User.objects.count() == 2)
 
-    # def test_photos_are_created(self):
-    #     """."""
-    #     self.assertTrue(Photo.objects.count() == 3)
+    def test_setup(self):
+        """."""
+        self.assertTrue(User.objects.count() == 2)
 
-    # def test_albums_are_created(self):
-    #     """."""
-    #     self.assertTrue(Album.objects.count() == 2)
+    def test_photos_are_created(self):
+        """."""
+        self.assertTrue(Photo.objects.count() == 3)
 
-    # def test_album1_photo(self):
-    #     """."""
-    #     self.assertTrue(Album.objects.get(title='First').photo.get(title="title"))
+    def test_albums_are_created(self):
+        """."""
+        self.assertTrue(Album.objects.count() == 2)
 
-#     def test_individual_photos_route(self):
-#         """Test the detail view for a photo view shows url for image."""
-#         number = Photo.objects.get(title='title').id
-#         c = Client()
-#         url = '/images/photos/' + str(number)
-#         response = c.get(url)
-#         self.assertTrue(b'title' in response.content)
+    def test_album1_photo(self):
+        """."""
+        self.assertTrue(Album.objects.get(title='First').photo.get(title="title"))
 
-#     def test_individual_photos_route2(self):
-#         """Test the detail view for a photo shows first and last name."""
-#         number = Photo.objects.get(title='title').id
-#         c = Client()
-#         url = '/images/photos/' + str(number)
-#         response = c.get(url)
-#         self.assertTrue(b'Bob Boberts' in response.content)
+    def test_individual_photos_route(self):
+        """Test the detail view for a photo view shows url for image."""
+        number = Photo.objects.get(title='title').id
+        c = Client()
+        url = '/images/photos/' + str(number)
+        response = c.get(url)
+        self.assertTrue(b'title' in response.content)
 
-#     def test_individual_photos_route3(self):
-#         """Test the detail view for a photo shows title of image."""
-#         number = Photo.objects.get(title='title').id
-#         c = Client()
-#         url = '/images/photos/' + str(number)
-#         response = c.get(url)
-#         self.assertTrue(b'title' in response.content)
+    def test_individual_photos_route2(self):
+        """Test the detail view for a photo shows first and last name."""
+        number = Photo.objects.get(title='title').id
+        c = Client()
+        url = '/images/photos/' + str(number)
+        response = c.get(url)
+        self.assertTrue(b'Bob Boberts' in response.content)
 
-#     def test_individual_photos_route4(self):
-#         """Test the detail view for a photo shows description."""
-#         number = Photo.objects.get(title='title').id
-#         c = Client()
-#         url = '/images/photos/' + str(number)
-#         response = c.get(url)
-#         self.assertTrue(b'Elephant' in response.content)
+    def test_individual_photos_route3(self):
+        """Test the detail view for a photo shows title of image."""
+        number = Photo.objects.get(title='title').id
+        c = Client()
+        url = '/images/photos/' + str(number)
+        response = c.get(url)
+        self.assertTrue(b'title' in response.content)
 
-#     def test_gallery_view_shows_only_public_photos(self):
-#         """Test the detail view for a photo shows description."""
-#         c = Client()
-#         url = '/images/photos/'
-#         response = c.get(url)
-#         self.assertTrue(len(response.context['display_photos']) == 2)
+    def test_individual_photos_route4(self):
+        """Test the detail view for a photo shows description."""
+        number = Photo.objects.get(title='title').id
+        c = Client()
+        url = '/images/photos/' + str(number)
+        response = c.get(url)
+        self.assertTrue(b'Elephant' in response.content)
 
-#     def test_gallery_view_shows_multiple_users_photos(self):
-#         """Test the detail view for a photo shows description."""
-#         c = Client()
-#         url = '/images/photos/'
-#         response = c.get(url)
-#         for photo in response.context['display_photos']:
-#             self.assertTrue(photo.published == "PBL")
+    def test_gallery_view_shows_only_public_photos(self):
+        """Test the detail view for a photo shows description."""
+        c = Client()
+        url = '/images/photos/'
+        response = c.get(url)
+        self.assertTrue(len(response.context['display_photos']) == 2)
 
-#     def test_album_gallery_only_shows_public(self):
-#         """Ablum view for all album only shows public."""
-#         c = Client()
-#         url = '/images/albums/'
-#         response = c.get(url)
-#         self.assertTrue(b'First' in response.content)
-#         self.assertTrue(b'Second' not in response.content)
+    def test_gallery_view_shows_multiple_users_photos(self):
+        """Test the detail view for a photo shows description."""
+        c = Client()
+        url = '/images/photos/'
+        response = c.get(url)
+        for photo in response.context['display_photos']:
+            self.assertTrue(photo.published == "PBL")
 
-#     def test_individual_album_response_200(self):
-#         """Ablum view for all album only shows public."""
-#         number = Album.objects.get(title='First').id
-#         c = Client()
-#         url = '/images/albums/' + str(number)
-#         response = c.get(url)
-#         self.assertTrue(response.status_code == 200)
+    def test_album_gallery_only_shows_public(self):
+        """Ablum view for all album only shows public."""
+        c = Client()
+        url = '/images/albums/'
+        response = c.get(url)
+        self.assertTrue(b'First' in response.content)
+        self.assertTrue(b'Second' not in response.content)
 
-#     def test_individual_album_shows_title(self):
-#         """Ablum view for all album only shows public."""
-#         number = Album.objects.get(title='First').id
-#         c = Client()
-#         url = '/images/albums/' + str(number)
-#         response = c.get(url)
-#         self.assertTrue(b"First" in response.content)
+    def test_individual_album_response_200(self):
+        """Ablum view for all album only shows public."""
+        number = Album.objects.get(title='First').id
+        c = Client()
+        url = '/images/albums/' + str(number)
+        response = c.get(url)
+        self.assertTrue(response.status_code == 200)
 
-#     def test_post_to_create_photo(self):
-#         """A post request to the add photo route should add it to db."""
-#         file = open(os.path.join(BASE_DIR, 'MEDIA/images/dreams.jpg'), 'rb')
-#         photo = SimpleUploadedFile('dreams.jpg', file.read())
-#         user1 = ImagerProfile.active.first()
-#         print('testt')
-#         self.client.post(reverse_lazy('photo_form'),
-#                          {'title': 'Whaddup',
-#                           'description': 'I made this',
-#                           'published': 'PBL',
-#                           'user': user1.id,
-#                           'image': photo})
-#         output = Photo.objects.get(title='Whaddup')
-#         self.assertTrue(output)
+    def test_individual_album_shows_title(self):
+        """Ablum view for all album only shows public."""
+        number = Album.objects.get(title='First').id
+        c = Client()
+        url = '/images/albums/' + str(number)
+        response = c.get(url)
+        self.assertTrue(b"First" in response.content)
 
-#     def test_post_new_photo_redirects_to_library(self):
-#         """A post request to the add photo route should add it to db."""
-#         file = open(os.path.join(BASE_DIR, 'MEDIA/images/dreams.jpg'), 'rb')
-#         photo = SimpleUploadedFile('dreams.jpg', file.read())
-#         user1 = ImagerProfile.active.first()
-#         res = self.client.post(reverse_lazy('photo_form'),
-#                                {'title': 'Whaddup',
-#                                 'description': 'I made this',
-#                                 'published': 'PBL',
-#                                 'user': user1.id,
-#                                 'image': photo})
+    def test_post_to_create_photo(self):
+        """A post request to the add photo route should add it to db."""
+        file = open(os.path.join(BASE_DIR, 'MEDIA/images/dreams.jpg'), 'rb')
+        photo = SimpleUploadedFile('dreams.jpg', file.read())
+        user1 = ImagerProfile.active.first()
+        print('testt')
+        self.client.post(reverse_lazy('photo_form'),
+                         {'title': 'Whaddup',
+                          'description': 'I made this',
+                          'published': 'PBL',
+                          'user': user1.id,
+                          'image': photo})
+        output = Photo.objects.get(title='Whaddup')
+        self.assertTrue(output)
 
-#         self.assertTrue(res.url == '/images/library/')
+    def test_post_new_photo_redirects_to_library(self):
+        """A post request to the add photo route should add it to db."""
+        file = open(os.path.join(BASE_DIR, 'MEDIA/images/dreams.jpg'), 'rb')
+        photo = SimpleUploadedFile('dreams.jpg', file.read())
+        user1 = ImagerProfile.active.first()
+        res = self.client.post(reverse_lazy('photo_form'),
+                               {'title': 'Whaddup',
+                                'description': 'I made this',
+                                'published': 'PBL',
+                                'user': user1.id,
+                                'image': photo})
 
-#     def test_post_to_create_album(self):
-#         """A post request to the add album route should add it to db."""
-#         user1 = ImagerProfile.active.first()
-#         print('testt')
-#         self.client.post(reverse_lazy('album_form'),
-#                          {'title': 'Thriller',
-#                           'description': 'I made this',
-#                           'published': 'PBL',
-#                           'user': user1.id})
-#         output = Album.objects.get(title='Thriller')
-#         self.assertTrue(output)
+        self.assertTrue(res.url == '/images/library/')
 
-#     def test_post_new_album_redirects_to_library(self):
-#         """A post request to the add photo route should add it to db."""
-#         user1 = ImagerProfile.active.first()
-#         res = self.client.post(reverse_lazy('album_form'),
-#                                {'title': 'Thriller',
-#                                 'description': 'I made this',
-#                                 'published': 'PBL',
-#                                 'user': user1.id})
+    def test_post_to_create_album(self):
+        """A post request to the add album route should add it to db."""
+        user1 = ImagerProfile.active.first()
+        print('testt')
+        self.client.post(reverse_lazy('album_form'),
+                         {'title': 'Thriller',
+                          'description': 'I made this',
+                          'published': 'PBL',
+                          'user': user1.id})
+        output = Album.objects.get(title='Thriller')
+        self.assertTrue(output)
 
-#         self.assertTrue(res.url == '/images/library/')
+    def test_post_new_album_redirects_to_library(self):
+        """A post request to the add photo route should add it to db."""
+        user1 = ImagerProfile.active.first()
+        res = self.client.post(reverse_lazy('album_form'),
+                               {'title': 'Thriller',
+                                'description': 'I made this',
+                                'published': 'PBL',
+                                'user': user1.id})
 
-#     def test_post_to_update_photo(self):
-#         """A post request to update photo route edits it in the db."""
-#         file = open(os.path.join(BASE_DIR, 'MEDIA/images/dreams.jpg'), 'rb')
-#         image = SimpleUploadedFile('dreams.jpg', file.read())
-#         photo = Photo.objects.first()
-#         user1 = ImagerProfile.active.first()
-#         self.client.post(reverse_lazy('edit_photo', kwargs={'pk': photo.id}),
-#                          {'title': 'Bad',
-#                           'description': 'changed',
-#                           'published': 'PBL',
-#                           'user': user1.id,
-#                           'image': image})
-#         self.assertTrue(Photo.objects.get(id=photo.id).title == "Bad")
+        self.assertTrue(res.url == '/images/library/')
 
-#     def test_post_to_update_photo_redirects_to_library(self):
-#         """A post request to update photo route edits it in the db."""
-#         file = open(os.path.join(BASE_DIR, 'MEDIA/images/dreams.jpg'), 'rb')
-#         image = SimpleUploadedFile('dreams.jpg', file.read())
-#         photo = Photo.objects.first()
-#         user1 = ImagerProfile.active.first()
-#         res = self.client.post(reverse_lazy('edit_photo',
-#                                kwargs={'pk': photo.id}),
-#                                {'title': 'Bad',
-#                                 'description': 'changed',
-#                                 'published': 'PBL',
-#                                 'user': user1.id,
-#                                 'image': image})
-#         self.assertTrue(res.url == '/images/library/')
+    def test_post_to_update_photo(self):
+        """A post request to update photo route edits it in the db."""
+        file = open(os.path.join(BASE_DIR, 'MEDIA/images/dreams.jpg'), 'rb')
+        image = SimpleUploadedFile('dreams.jpg', file.read())
+        photo = Photo.objects.first()
+        user1 = ImagerProfile.active.first()
+        self.client.post(reverse_lazy('edit_photo', kwargs={'pk': photo.id}),
+                         {'title': 'Bad',
+                          'description': 'changed',
+                          'published': 'PBL',
+                          'user': user1.id,
+                          'image': image})
+        self.assertTrue(Photo.objects.get(id=photo.id).title == "Bad")
 
-#     def test_post_to_update_album(self):
-#         """A post request to update photo route edits it in the db."""
-#         album = Album.objects.first()
-#         user1 = ImagerProfile.active.first()
-#         self.client.post(reverse_lazy('edit_album', kwargs={'pk': album.id}),
-#                          {'title': 'Bad',
-#                           'description': 'changed',
-#                           'published': 'PBL',
-#                           'user': user1.id})
-#         self.assertTrue(Album.objects.get(id=album.id).title == "Bad")
+    def test_post_to_update_photo_redirects_to_library(self):
+        """A post request to update photo route edits it in the db."""
+        file = open(os.path.join(BASE_DIR, 'MEDIA/images/dreams.jpg'), 'rb')
+        image = SimpleUploadedFile('dreams.jpg', file.read())
+        photo = Photo.objects.first()
+        user1 = ImagerProfile.active.first()
+        res = self.client.post(reverse_lazy('edit_photo',
+                               kwargs={'pk': photo.id}),
+                               {'title': 'Bad',
+                                'description': 'changed',
+                                'published': 'PBL',
+                                'user': user1.id,
+                                'image': image})
+        self.assertTrue(res.url == '/images/library/')
 
-#     def test_post_to_update_album_redirects_to_library(self):
-#         """A post request to update photo route edits it in the db."""
-#         album = Album.objects.first()
-#         user1 = ImagerProfile.active.first()
-#         res = self.client.post(reverse_lazy('edit_album',
-#                                kwargs={'pk': album.id}),
-#                                {'title': 'Bad',
-#                                 'description': 'changed',
-#                                 'published': 'PBL',
-#                                 'user': user1.id})
-#         self.assertTrue(res.url == '/images/library/')
+    def test_post_to_update_album(self):
+        """A post request to update photo route edits it in the db."""
+        album = Album.objects.first()
+        user1 = ImagerProfile.active.first()
+        self.client.post(reverse_lazy('edit_album', kwargs={'pk': album.id}),
+                         {'title': 'Bad',
+                          'description': 'changed',
+                          'published': 'PBL',
+                          'user': user1.id})
+        self.assertTrue(Album.objects.get(id=album.id).title == "Bad")
+
+    def test_post_to_update_album_redirects_to_library(self):
+        """A post request to update photo route edits it in the db."""
+        album = Album.objects.first()
+        user1 = ImagerProfile.active.first()
+        res = self.client.post(reverse_lazy('edit_album',
+                               kwargs={'pk': album.id}),
+                               {'title': 'Bad',
+                                'description': 'changed',
+                                'published': 'PBL',
+                                'user': user1.id})
+        self.assertTrue(res.url == '/images/library/')
 
 
 """
--- imagersite/imagersite/tests.py
+++ imagersite/imagersite/tests.py
@@ -10,52 +10,52 @@ import factory
 import pdb
 
 
-# class ProfileTestCase(TestCase):
-#     """."""
+class ProfileTestCase(TestCase):
+    """."""
 
-#     def test_home_route_response_200(self):
-#         """."""
-#         c = Client()
-#         response = c.get('/')
-#         self.assertTrue(response.status_code == 200)
+    def test_home_route_response_200(self):
+        """."""
+        c = Client()
+        response = c.get('/')
+        self.assertTrue(response.status_code == 200)
 
-#     def test_login_route_response_200(self):
-#         """."""
-#         c = Client()
-#         response = c.get('/accounts/login/')
-#         self.assertTrue(response.status_code == 200)
+    def test_login_route_response_200(self):
+        """."""
+        c = Client()
+        response = c.get('/accounts/login/')
+        self.assertTrue(response.status_code == 200)
 
-#     def test_register_route_response_200(self):
-#         """."""
-#         c = Client()
-#         response = c.get('/accounts/register/')
-#         self.assertTrue(response.status_code == 200)
+    def test_register_route_response_200(self):
+        """."""
+        c = Client()
+        response = c.get('/accounts/register/')
+        self.assertTrue(response.status_code == 200)
 
-#     def test_mailbox_empty(self):
-#         """."""
-#         self.assertEqual(len(mail.outbox), 0)
+    def test_mailbox_empty(self):
+        """."""
+        self.assertEqual(len(mail.outbox), 0)
 
-#     def test_register_post_new_account_redirects(self):
-#         """."""
-#         c = Client()
-#         response = c.post('/accounts/register', {'username': 'test1',
-#                                                  'email': 'test@test.com',
-#                                                  'password1': 'UltraTest',
-#                                                  'password2': 'UltraTest'}, follow=True)
-#         self.assertTrue(response.status_code == 200)
+    def test_register_post_new_account_redirects(self):
+        """."""
+        c = Client()
+        response = c.post('/accounts/register', {'username': 'test1',
+                                                 'email': 'test@test.com',
+                                                 'password1': 'UltraTest',
+                                                 'password2': 'UltraTest'}, follow=True)
+        self.assertTrue(response.status_code == 200)
 
-#     def test_homepage_has_photos_in_db(self):
-#         """Homepage shows photos when they exist in database."""
-#         User.objects.create(username='Bob')
-#         user1 = User.objects.get(username='Bob')
-#         profile1 = ImagerProfile.active.create(user=user1, website='test', location='here', bio='isuck', phone='phone', fee=10, services='WD', photo_styles='BW')
-#         Photo.objects.create(title='title', description=' ', published='PBL', user=profile1, image='https://i.vimeocdn.com/portrait/58832_300x300')
-#         c = Client()
-#         response = c.get('/')
-#         self.assertTrue(b"/media/https%3A/i.vimeocdn.com/portrait/58832_300x300" in response.content)
+    def test_homepage_has_photos_in_db(self):
+        """Homepage shows photos when they exist in database."""
+        User.objects.create(username='Bob')
+        user1 = User.objects.get(username='Bob')
+        profile1 = ImagerProfile.active.get(user=user1)
+        Photo.objects.create(title='title', description=' ', published='PBL', user=profile1, image='https://i.vimeocdn.com/portrait/58832_300x300')
+        c = Client()
+        response = c.get('/')
+        self.assertTrue(b"/media/https%3A/i.vimeocdn.com/portrait/58832_300x300" in response.content)
 
-#     def test_homepage_has_placeholder_no_images_in_db(self):
-#         """Homepage shows placeholder images when no images in database."""
-#         c = Client()
-#         response = c.get('/')
-#         self.assertTrue(b"http://placehold.it/900x350" in response.content)
+    def test_homepage_has_placeholder_no_images_in_db(self):
+        """Homepage shows placeholder images when no images in database."""
+        c = Client()
+        response = c.get('/')
+        self.assertTrue(b"http://placehold.it/900x350" in response.content)
